734bb4040952b7ef5a0fcd40a5d8d233b582fd94
Rafael de Luca <deluca1712@gmail.com>
2024-03-29 19:32:43 -0300

    Agile definition

---
+++ src/agile/introducation_to_agile/Definition.java
@@ -0,0 +1,26 @@
+package agile.introducation_to_agile;
+
+public class Definition {
+
+	public static void main(String[] args) {
+
+		System.out.println("Waterfall Process"
+				+ "\nFrom gathering the requirements to desing (can not go back on stages)."
+				+ "\nTalk to customers -> Desing product -> Start Production -> Test -> Deliver."
+				+ "\nWaterfall does not mean something bad, sometimes we can go back to previous phase during the production process."
+				+ "\nIs it indecate when we can make no mistakes and define the requiriments very precisely at the beginning."
+				+ "\nAgile"
+				+ "\nApproach a production of products knowing that things will change."
+				+ "\nWe can iterate throught the software production process."
+				+ "\nAgile is a mindset, not a framework ou a tool."
+				+ "\nAgile is a mindset that allows we to itetate quickly and adapt to changing."
+				+ "\nAlow the team to learn from the mistakes and deliver the product that the cliente really want."
+				+ "");
+		
+		System.out.println("\nAdapting to changint requirements."
+				+ "\nHave to adapt and make changes or die."
+				+ "\nProcuts that was famous in the past die because they do not adapt to what clientes really want."
+				+ "\nException: sometimes change is really expensive and we can go back on stages.");
+	}
+
+}
