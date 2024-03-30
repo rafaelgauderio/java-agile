7290e5ce689c9babf845b3689fe6cc2d28eb720b
Rafael de Luca <deluca1712@gmail.com>
2024-03-29 21:20:45 -0300

    The 12 principles and 4 values of agile process

---
+++ src/agile/introducation_to_agile/PrinciplesAndValues.java
@@ -0,0 +1,29 @@
+package agile.introducation_to_agile;
+
+public class PrinciplesAndValues {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n4 Values"
+				+ "\nindividuals and interactions over processes and tools."
+				+ "\nWorking product that brings value over comprehensive documentation"
+				+ "\nCustomer collaboration over contract negotiation."
+				+ "\nResponding to change over following a plan");
+		
+		System.out.println("\n12 "
+				+ "Principles"
+				+ "\nThe highest priority is the customer satisfy through early and continuous delivery of valuable software."
+				+ "\nWelcome changing requirements, even late in development."
+				+ "\nDeliver working frequently, preferably daily."
+				+ "\nBuild project around motivated individuals. Trust them to do the job."
+				+ "\nBusiness people and developers must work together daily throughout the project."
+				+ "\nThe most efficient and effective method os comunication is face-to-face conversation."
+				+ "\nDelivering incremental value to the customer."
+				+ "\nAgile processes promote sustainable development."
+				+ "\nContinuous attention to technical excellence and good desing enhances agility."
+				+ "\nSimplicity: the art of maximizing the amount of work no done is essential."
+				+ "\nThe best architetures, requirements and desings emerge from self-organizing teams."
+				+ "\nAt regular invervals, the team reflects to become more effective, then tunes and adjusts its behavior accordingly.");
+	}
+
+}
