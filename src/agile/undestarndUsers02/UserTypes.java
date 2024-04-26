20387e7bad22fbb91768edb36de08953f8fe83a5
Rafael de Luca <deluca1712@gmail.com>
2024-04-26 16:03:09 -0300

    users types

---
+++ src/agile/undestarndUsers02/UserTypes.java
@@ -0,0 +1,22 @@
+package agile.undestarndUsers02;
+
+public class UserTypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("The aim in the agile is to deliver value to users (clients) as soon as possible.\n"
+				+ "To deliver value we must deeply undertand who the users are.\n"
+				+ "Even niche users can be broken down further into users types.\n"
+				+ "The agile coach has guide de product manager and product owner to identifying user types and bulding user personas.\n");
+		
+		System.out.println("User types\n"
+				+ "\t1. User role on application/site (buyer, seller, admin, customer).\n"
+				+ "\t2. Demographics (age, gender, county).\n"
+				+ "\t3. Tier/rank (free, standard, premium).\n"
+				+ "\t4. Platfomr (Android, Windows, Mac).\n"
+				+ "\t5. Language.\n"
+				+ "\t6. Other factor (hobbies, amount of free time).\n");		
+		
+	}
+
+}
