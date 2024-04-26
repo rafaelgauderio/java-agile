c1325ba56a5eaddd412f0ff67c80641771bb4e66
Rafael de Luca <deluca1712@gmail.com>
2024-04-26 15:28:36 -0300

    agile team

---
+++ src/agile/introducation_to_agile/AgileTeam.java
@@ -0,0 +1,24 @@
+package agile.introducation_to_agile;
+
+public class AgileTeam {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("1. Product Owner: CEO of product (make all the decisions for the product).\n"
+				+ "\tProduct Manager: Executor. Still has to consult higher management about importants decisions.\n"
+				+ "2. Engineers: frontend and backend development. Ideal to have full stack enginners.\n"
+				+ "3. Quality Assurance: engineers that the focus is to write automated tests.\n"
+				+ "4. UX (User Experience): Visual Desing and user experience.\n"
+				+ "5. Prototypers: \n"
+				+ "6. Marketind, Data Science: \n"
+				+ "7. Scrum Master: Facilitate daily processes and support the team to adhering to agile practices.\n");
+		
+		System.out.println("Agile Coach.\n"
+				+ "\tHas to deeply understand the product, team´s dynamics, processes and how roles are distributed.\n"
+				+ "\tHe is not part of the team.\n"
+				+ "\tBe respecful and gentle.\n"
+				+ "\tPay attention to body language.\n");
+	}
+
+}
