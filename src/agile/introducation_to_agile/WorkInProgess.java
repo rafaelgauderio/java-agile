ea9fd4cbc247fa05fd3b9666a3b41e5b9fee5dea
Rafael de Luca <deluca1712@gmail.com>
2024-04-26 15:42:32 -0300

    work in progress

---
+++ src/agile/introducation_to_agile/WorkInProgess.java
@@ -0,0 +1,16 @@
+package agile.introducation_to_agile;
+
+public class WorkInProgess {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Agile shoud minimizing work in progress. Help the team to move faster and more efficiently.\n"
+				+ "Less works (tasks) in progess, more fast we can move as a team.\n"
+				+ "Bases on Little´s law.\n"
+				+ "L = lambda * W.\n"
+				+ "L = work in progress, lambda = throughtput rate, W = cycle time.\n"
+				+ "Less work tasks in progress, the delivery time por task decreases significantly.\n"
+				+ "Reducing work in progress also helps to find blockers. Multiple tasks are very unproductive.");
+	}
+
+}
