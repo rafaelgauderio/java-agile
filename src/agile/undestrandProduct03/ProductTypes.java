d24390772cef882427bbaeb09b08f042c3b8c022
Rafael de Luca <deluca1712@gmail.com>
2024-05-02 15:53:42 -0300

    mvp

--- src/agile/undestrandProduct03/ProductTypes.java
+++ src/agile/undestrandProduct03/ProductTypes.java
@@ -15,7 +15,14 @@ public class ProductTypes {
 				+ "\tSoftware has a lot of flexibility, but the competition is also very hard.\n"
 				+ "4. Combined Products.\n"
 				+ "\tSAAS - software as a service (AWS).\n"
-				+ "\tProducts with software integrated");
+				+ "\tProducts with software integrated\n");
+		
+		System.out.println("MVP\n"
+				+ "\tMinimum viable product.\n"
+				+ "\tIt is the smallest possible product with a limited amount of features that you can possibly build that customers will get some value.\n"
+				+ "\tUsed to validate hypothesis about users.\n"
+				+ "\tIt is possible to manually do things behind the scenes without building the entire product.\n"
+				+ "\tIt might be discarded, so do not make promises to customers.\n");
 
 	}
 
