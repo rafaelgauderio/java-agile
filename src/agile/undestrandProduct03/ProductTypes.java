82c8ef64e64a135f566e9d2d602aefed471592b0
Rafael de Luca <deluca1712@gmail.com>
2024-05-02 15:44:34 -0300

    Product Types

---
+++ src/agile/undestrandProduct03/ProductTypes.java
@@ -0,0 +1,22 @@
+package agile.undestrandProduct03;
+
+public class ProductTypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println(
+				"1. Products\n"
+				+ "\tTangigle goods (pyshical products).\n"
+				+ "2. Services\n"
+				+ "\tIntangigle value.\n"
+				+ "3. Software.\n"
+				+ "\tIntangigle value through product.\n"
+				+ "\tSoftware development is unique in a way that it combines product and service.\n"
+				+ "\tSoftware has a lot of flexibility, but the competition is also very hard.\n"
+				+ "4. Combined Products.\n"
+				+ "\tSAAS - software as a service (AWS).\n"
+				+ "\tProducts with software integrated");
+
+	}
+
+}
